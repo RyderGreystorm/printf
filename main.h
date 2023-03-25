16cf89b2b2c9a574bf872f89490317431241a0ac
Godbless Biekro <biekrogodbless@gmail.com>
2023-03-25 21:22:37 +0000

    updated main.h

--- main.h
+++ main.h
@@ -9,5 +9,7 @@
 #include <fcntl.h>
 
 int _printf(const char *format, ...);
-
+char *replace_with_per(char *s, int i);
+char *replace_with_string(char *s, int i, char *str);
+char *replace_with_char(char *s, int i, char c);
 #endif /**MAIN_H*/
