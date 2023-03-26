85cd38c6fb0bce5e7c5f2dda51af847e2a895409
Robert Amoah <mr_robertamoah@yahoo.com>
2023-03-26 12:13:21 +0100

    made changes to replace_with_char.c

--- test/0-main.c
+++ test/0-main.c
@@ -4,14 +4,9 @@ int main(void)
 {
 int a;
     _printf("The first %% char is: %c. Here is the string: %s\n the third string: %s and %%\n", 'k', "HELLO", "WORLD");
-_printf("hey _p: %c%c\n", 'o', 'm');    
-printf("hey p: %c%c\n", 'o', 'm');    
-_printf("hey _p: %c%c\n");    
-printf("hey p: %c%c\n");    
-_printf(NULL);
-a = printf(NULL);
-printf("a: %d\n", a);
-printf("The first char is p: %c\n", "hello");
-    _printf("The first char is _p: %c\n", "hello");
+a = _printf("hey string: %c\n", 2000);    
+printf("_pf a is: %d\n", a);
+a = printf("hey string: %c\n", 2000);    
+printf("pf a is: %d\n", a);
     return (0);
 }
